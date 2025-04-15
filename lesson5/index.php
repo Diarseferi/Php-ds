94a61bc8f846af7058972080a21194afab6733c6
Diarseferi <diarseferi23@gmail.com>
2025-04-15 16:03:45 +0200

    lesson5.1

--- lesson5/index.php
+++ lesson5/index.php
@@ -7,23 +7,66 @@
 </head>
 <body>
     <?php
-    $dogs=array(
-        array("Husky","siberise", 15),
-        array("Husky","siberise", 15),
-        array("Husky","siberise", 15),
-    );
+    // $dogs=array(
+    //     array("Husky","siberise", 15),
+    //     array("Husky","siberise", 15),
+    //     array("Husky","siberise", 15),
+    // );
 
-    $dogs[0][0].": orgin:".$dogs[0][1].": life span;".$dogs[0][2]."<br>";
-    $dogs[1][0].": orgin:".$dogs[1][1].": life span;".$dogs[1][2]."<br>";
-    $dogs[2][0].": orgin:".$dogs[2][1].": life span;".$dogs[2][2]."<br>";
+    // $dogs[0][0].": orgin:".$dogs[0][1].": life span;".$dogs[0][2]."<br>";
+    // $dogs[1][0].": orgin:".$dogs[1][1].": life span;".$dogs[1][2]."<br>";
+    // $dogs[2][0].": orgin:".$dogs[2][1].": life span;".$dogs[2][2]."<br>";
 
-    for($row=0;$row<3;$row++){
-        echo "<p><b> row Number $row </b></p>";
-        echo "<ul>";
-        for($column=0;$column<3;$column++){
-            echo "<li>" .$dogs[$row][$col];
-        }
-        echo "</ul>";
+    // for($row=0;$row<3;$row++){
+    //     echo "<p><b> row Number $row </b></p>";
+    //     echo "<ul>";
+    //     for($column=0;$column<3;$column++){
+    //         echo "<li>" .$dogs[$row][$col];
+    //     }
+    //     echo "</ul>";
+    // }
+     ?>
+     <?php
+    //  $array=array(
+    //     array(1,2,3),
+    //     array(1,2,3),
+    //     array(1,2,3),
+    //  );
+
+    //  for($i=0,$i<4,$i++){
+    //     for($j=0,$j<4,$j++){
+    //         echo " array $i element $j <br>";
+    //     }
+
+    //  }
+    // for($i=0;$i<5;$i++){
+    //     for($j=$i;$i<5;$j++){
+    //         echo "&nbsp";
+    //     }
+    //     for($j=1;$j<=$i;$j++){
+    //         echo'*';
+    //     }
+    //     echo "<br>";
+    // }
+     ?>
+     <?php
+     // asociative loops
+    //  $grade=array(
+    //     "Math"=>"3",
+    //     "History"=>"2",
+    //     "Art"=>"3",
+    //     "Chemistry"=>"4",
+    //  )
+
+    //  echo = "Math grade is"+.$grade['Math'];
+    // // $grade=['Math']='3'
+    // // $grade=['Art']='2',
+    // // $grade=['Music']='3',
+    // // $grade=['History']='4',
+
+    // foreach($grade as $subject  =>$grade){
+    //     echo "Subject".$subject."Grade:".$grade;
+    //     echo"<br>";
     }
      ?>
 </body>
