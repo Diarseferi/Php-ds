03013bee38201b0df94fdeae6aeceecf40225067
Diarseferi <diarseferi23@gmail.com>
2025-04-17 16:09:27 +0200

    lesson6

--- lesson5/index.php
+++ lesson5/index.php
@@ -67,7 +67,7 @@
     // foreach($grade as $subject  =>$grade){
     //     echo "Subject".$subject."Grade:".$grade;
     //     echo"<br>";
-    }
+    // }
      ?>
 </body>
 </html>
---
+++ lesson6/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lesson6</title>
+</head>
+<body>
+    <?php
+//     $my_file=fopen("ds.txt","r");
+    
+// fclose($my_file);
+//  w- write only mode
+
+
+// r - read only mode
+
+
+// a - write only mode. Te dhenat ne files ruhen
+
+
+// w+ , r+ , a+ 
+// $myfilename=("ds.txt");
+
+// $myfile=($myfilename,'r');
+
+// $size=filesize($myfilename);
+
+// $myfiledata= fread($myfile,$size);
+
+
+// $file= fopen("ds.txt" , "r");
+
+// while(!feof($file)){
+//     echo fgets($file)."<br>";
+// }
+   
+// fclose($my_file);
+
+// $my_file=fopen("ds.txt","r");
+
+// $mytext="Digital School /n";
+
+// fwrite($my_file,$mytext);
+
+// file_put_contents('ds.txt',"Add more text");
+
+// echo  file_get_contents("ds.txt");
+
+
+    ?>
+</body>
+</html>
