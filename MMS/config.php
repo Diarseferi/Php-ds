fd099a1c7b0cd0704efb20891c7f0dd2fe4c6f75
Diarseferi <diarseferi23@gmail.com>
2025-06-05 16:10:12 +0200

    lesson16

---
+++ MMS/config.php
@@ -0,0 +1,20 @@
+<?php
+
+$user = "root";
+$pass="";
+$server = "localhost";
+$dbname = "mms";
+
+try{
+
+    $conn - new PDO("mysql:host=$server;dbname=$dbname", $user, $pass);
+
+}catch(PDOExpection $e){
+
+    echo "Error: " . $e->getMessage();
+}
+
+
+
+
+?>
